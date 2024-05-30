726b7fc3886abcabf9994045b5c3aca6043295d7
Andrew Mee <andrew@midi2.dev>
2024-05-30 12:39:23 +1000

    bytestreamToUMP and umpToBytestream are both header only files. Request from Amenote

--- include/bytestreamToUMP.h
+++ include/bytestreamToUMP.h
@@ -20,11 +20,15 @@
 
 #ifndef BSUMP_H
 #define BSUMP_H
+#include <cstdint>
 
 #define BSTOUMP_BUFFER 4
 
+#ifndef clear
+#define clear(dest, c,n ) for(uint16_t i = 0 ; i < n ; i ++) dest[i] = c;
+#endif
 
-#include <cstdint>
+#include "utils.h"
 
 class bytestreamToUMP{
 
@@ -45,28 +49,235 @@ class bytestreamToUMP{
 		uint8_t rpnMsbValue[16];
 		uint8_t rpnMsb[16];
 		uint8_t rpnLsb[16];
-	    	
-		void bsToUMP(uint8_t b0, uint8_t b1, uint8_t b2);
-		void increaseWrite();
-
 		int readIndex = 0;
 		int writeIndex = 0;
 		int bufferLength = 0;
+	    	
+		void bsToUMP(uint8_t b0, uint8_t b1, uint8_t b2){
+		  uint8_t status = b0 & 0xF0;
 
+		   if(b0 >= TIMING_CODE){
+			  umpMess[writeIndex] = ((UMP_SYSTEM << 4) + defaultGroup + 0L) << 24;
+			  umpMess[writeIndex] +=  (b0 + 0L) << 16;
+			  umpMess[writeIndex] +=  b1  << 8;
+			  umpMess[writeIndex] +=  b2;
+   			increaseWrite();
+		   }else if(status>=NOTE_OFF && status<=PITCH_BEND){
+			  if(outputMIDI2){
+				  uint8_t channel = b0 & 0xF;
+
+				  if(status==NOTE_ON && b2==0){
+					 status=NOTE_OFF;
+		             b2 = 0x40;
+				  }
+
+				  umpMess[writeIndex] = ((UMP_M2CVM << 4) + defaultGroup + 0L) << 24;
+				  umpMess[writeIndex] += (status + channel + 0L)<<16;
+
+				  if(status==NOTE_ON || status==NOTE_OFF){
+					umpMess[writeIndex] += (b1 + 0L) <<8;
+		  			increaseWrite();
+		  			umpMess[writeIndex] = (M2Utils::scaleUp(b2,7,16) << 16);
+		  			increaseWrite();
+				  } else if (status == KEY_PRESSURE){
+					umpMess[writeIndex] += (b1 + 0L) <<8;
+		  			increaseWrite();
+		  			umpMess[writeIndex] = (M2Utils::scaleUp(b2,7,16) << 16);
+		  			increaseWrite();
+				  } else if (status == PITCH_BEND){
+		  			increaseWrite();
+		  			umpMess[writeIndex] = M2Utils::scaleUp((b1<<7) + b2,14,32);
+		  			increaseWrite();
+				  } else if (status == PROGRAM_CHANGE){
+					if(bankMSB[channel]!=255 && bankLSB[channel]!=255){
+						umpMess[writeIndex] += 1;
+						increaseWrite();
+						umpMess[writeIndex] += (bankMSB[channel] <<8) + bankLSB[channel ];
+					}
+					umpMess[writeIndex] += (b1 + 0L) << 24;
+		  			increaseWrite();
+				  } else if (status == CHANNEL_PRESSURE){
+		  			increaseWrite();
+		              umpMess[writeIndex] = M2Utils::scaleUp(b1,7,32);
+		  			increaseWrite();
+				  }  else if (status == CC){
+					switch(b1){
+					 case 0:
+						bankMSB[channel] = b2;
+						return;
+					 case 32:
+						bankLSB[channel] = b2;
+						return;
+
+					 case 6: //RPN MSB Value
+						if(rpnMsb[channel]==255 || rpnLsb[channel]==255){
+							return;
+						}
+
+						if(rpnMode[channel] && rpnMsb[channel] == 0 && (rpnLsb[channel] == 0 || rpnLsb[channel] == 6)){
+							status = rpnMode[channel]? RPN: NRPN;
+
+							umpMess[writeIndex] = ((UMP_M2CVM << 4) + defaultGroup + 0L) << 24;
+							umpMess[writeIndex] += (status + channel + 0L)<<16;
+							umpMess[writeIndex] += ((int)rpnMsb[channel]<<7) + rpnLsb[channel] + 0L;
+							increaseWrite();
+							umpMess[writeIndex] = M2Utils::scaleUp(((int)b2<<7),14,32);
+							increaseWrite();
+
+						}else{
+							rpnMsbValue[channel] = b2;
+							return;
+						}
+						break;
+					case 38: //RPN LSB Value
+						if(rpnMsb[channel]==255 || rpnLsb[channel]==255){
+							return;
+						}
+						status = rpnMode[channel]? RPN: NRPN;
+
+						umpMess[writeIndex] = ((UMP_M2CVM << 4) + defaultGroup + 0L) << 24;
+						umpMess[writeIndex] += (status  + channel + 0L)<<16;
+						umpMess[writeIndex] += ((int)rpnMsb[channel]<<7) + rpnLsb[channel] + 0L;
+						increaseWrite();
+						umpMess[writeIndex] = M2Utils::scaleUp(((int)rpnMsbValue[channel]<<7) + b2,14,32);
+						increaseWrite();
+						break;
+					case 99:
+						rpnMode[channel] = false;
+						rpnMsb[channel] = b2;
+						return;
+					case 98:
+						rpnMode[channel] = false;
+						rpnLsb[channel] = b2;
+						return;
+					case 101:
+						rpnMode[channel] = true;
+						rpnMsb[channel] = b2;
+						return;
+
+					case 100:
+						rpnMode[channel] = true;
+						rpnLsb[channel] = b2;
+						return;
+
+					default:
+						umpMess[writeIndex] += (b1 + 0L) <<8;
+						increaseWrite();
+						umpMess[writeIndex] = M2Utils::scaleUp(b2,7,32);
+						increaseWrite();
+						break;
+					}
+				  }
+
+
+			  }
+   			else {
+				  umpMess[writeIndex] = ((UMP_M1CVM << 4) + defaultGroup + 0L) << 24;
+				  umpMess[writeIndex] +=  (b0 + 0L) << 16;
+				  umpMess[writeIndex] +=  b1  << 8;
+				  umpMess[writeIndex] +=  b2;
+   				increaseWrite();
+		   }
+		  }
+		}
+
+		void increaseWrite(){
+			bufferLength++;
+			writeIndex++;
+			if (writeIndex == BSTOUMP_BUFFER) {
+				writeIndex = 0;
+			}
+		}
 
 	public:
 		uint8_t defaultGroup = 0;
 		bool outputMIDI2 = false;
 		
-   		bytestreamToUMP();
+		bytestreamToUMP(){
+			clear(bankMSB, 255, sizeof(bankMSB));
+			clear(bankLSB, 255, sizeof(bankLSB));
+			clear(rpnMsbValue, 255, sizeof(rpnMsbValue));
+			clear(rpnMsb, 255, sizeof(rpnMsb));
+			clear(rpnLsb, 255, sizeof(rpnLsb));
+		}
 		
-		bool availableUMP();
+		bool availableUMP(){
+			return bufferLength;
+		}
 		
-		uint32_t readUMP();
-		
-		void bytestreamParse(uint8_t midi1Byte);
+		uint32_t readUMP(){
+			uint32_t mess = umpMess[readIndex];
+			bufferLength--;	 //	Decrease buffer size after reading
+			readIndex++;
+			if (readIndex == BSTOUMP_BUFFER) {
+				readIndex = 0;
+			}
 
-	
+			return mess;
+		}
+		
+		void bytestreamParse(uint8_t midi1Byte){
+
+			if (midi1Byte == TUNEREQUEST || midi1Byte >=  TIMINGCLOCK) {
+				d0 = midi1Byte;
+				bsToUMP(midi1Byte,0,0);
+				return;
+			}
+
+			if (midi1Byte & NOTE_OFF) { // Status byte received
+				d0 = midi1Byte;
+				d1 = 255;
+
+				if (midi1Byte == SYSEX_START){
+					sysex7State = 1;
+					sysex7Pos = 0;
+				}else
+					if (midi1Byte == SYSEX_STOP){
+
+						umpMess[writeIndex] = ((UMP_SYSEX7 << 4) + defaultGroup + 0L) << 24;
+						umpMess[writeIndex] +=  ((sysex7State == 1?0:3) + 0L) << 20;
+						umpMess[writeIndex] +=  ((sysex7Pos + 0L) << 16) ;
+						umpMess[writeIndex] += (sysex[0] << 8) + sysex[1];
+						increaseWrite();
+						umpMess[writeIndex] = ((sysex[2] + 0L) << 24) + ((sysex[3] + 0L)<< 16) + (sysex[4] << 8) + sysex[5];
+						increaseWrite();
+
+						sysex7State = 0;
+						clear(sysex, 0, sizeof(sysex));
+					}
+			} else if(sysex7State >= 1){
+				//Check IF new UMP Message Type 3
+				if(sysex7Pos%6 == 0 && sysex7Pos !=0){
+					umpMess[writeIndex] = ((UMP_SYSEX7 << 4) + defaultGroup + 0L) << 24;
+					umpMess[writeIndex] +=  (sysex7State + 0L) << 20;
+					umpMess[writeIndex] +=  6L << 16;
+					umpMess[writeIndex] += (sysex[0] << 8) + sysex[1];
+					increaseWrite();
+					umpMess[writeIndex] = ((sysex[2] + 0L) << 24) + ((sysex[3] + 0L)<< 16) + (sysex[4] << 8) + sysex[5] + 0L;
+					increaseWrite();
+					clear(sysex, 0, sizeof(sysex));
+					sysex7State=2;
+					sysex7Pos=0;
+				}
+
+				sysex[sysex7Pos] = midi1Byte;
+				sysex7Pos++;
+			} else if (d1 != 255) { // Second byte
+				bsToUMP(d0, d1, midi1Byte);
+				d1 = 255;
+			} else if (d0){ // status byte set
+				if (
+				  (d0 & 0xF0) == PROGRAM_CHANGE
+				  || (d0 & 0xF0) == CHANNEL_PRESSURE
+				  || d0 == TIMING_CODE
+				  || d0 == SONG_SELECT
+				) {
+					bsToUMP(d0, midi1Byte, 0);
+				} else if (d0 < SYSEX_START || d0 == SPP) { // First data byte
+					d1=midi1Byte;
+				}
+			}
+		}
 };
 
 #endif
